8ee13c95803240df7d3762ff03dbba090b9366a6
kieransweetman <kikisweetman@gmail.com>
2024-08-26 14:38:35 +0200

    created test entity and db connection and added app config props to gitignore

---
+++ src/main/java/fr/diginamic/imdb/config/Config.java
@@ -0,0 +1,9 @@
+package fr.diginamic.imdb.config;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.diginamic.imdb.service.AbstractService;
+
+public class Config {
+
+}
---
+++ src/main/java/fr/diginamic/imdb/repository/TestRepository.java
@@ -0,0 +1,8 @@
+package fr.diginamic.imdb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.diginamic.imdb.entity.TestEntity;
+
+public interface TestRepository extends JpaRepository<TestEntity, Long> {
+}
