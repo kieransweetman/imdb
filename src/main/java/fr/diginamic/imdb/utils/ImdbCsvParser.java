459a9d08fcc4ca69589bef284c993544cf06791a
kieransweetman <kikisweetman@gmail.com>
2024-08-27 14:22:52 +0200

    cant import classes?? why?

--- src/main/java/fr/diginamic/imdb/utils/ImdbCsvParser.java
+++ src/main/java/fr/diginamic/imdb/utils/ImdbCsvParser.java
@@ -1,18 +1,37 @@
+package fr.diginamic.imdb.utils;
+
 import org.springframework.stereotype.Component;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+import fr.diginamic.imdb.strategies;
+
 @Component
 public class ImdbCsvParser {
     public void parseFiles(List<String> csvFilePaths) {
         for (String csvFilePath : csvFilePaths) {
+
+            CsvProcessingStrategyFactory strategy = CsvProcessingStrategyFactory.getStrategyForFile(csvFilePath);
+            if (strategy == null) {
+                ImdbException.log(new IllegalArgumentException("No strategy found for file: " + csvFilePath));
+                continue;
+            }
+
             try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
+
+                // skip headers
+                br.readLine();
+
                 String line;
                 while ((line = br.readLine()) != null) {
-                    // Process each line of the CSV file
-                    processLine(line);
+                    try {
+                        strategy.processLine(line);
+                    } catch (Exception e) {
+                        ImdbException.log(e);
+                    }
+
                 }
             } catch (IOException e) {
                 ImdbException.log(e);
@@ -20,10 +39,4 @@ public class ImdbCsvParser {
         }
     }
 
-    private void processLine(String line) {
-        // Implement your CSV line processing logic here
-        // For example, you can split the line by commas and process each field
-        String[] fields = line.split(",");
-        // Process fields
-    }
 }
