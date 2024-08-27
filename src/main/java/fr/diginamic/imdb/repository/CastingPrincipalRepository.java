3326f220433f08d80cbb7242b614cf6d3e04fbdb
kieransweetman <kikisweetman@gmail.com>
2024-08-27 15:13:48 +0200

    type error fix

--- src/main/java/fr/diginamic/imdb/repository/CastingPrincipalRepository.java
+++ src/main/java/fr/diginamic/imdb/repository/CastingPrincipalRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import fr.diginamic.imdb.entity.CastingPrincipal;
+import fr.diginamic.imdb.entity.CastingPrincipalId;
 
 @Repository
-public interface CastingPrincipalRepository extends JpaRepository<CastingPrincipal, Integer>{
+public interface CastingPrincipalRepository extends JpaRepository<CastingPrincipal, CastingPrincipalId> {
 
 }
--- src/main/java/fr/diginamic/imdb/service/CastingPrincipalService.java
+++ src/main/java/fr/diginamic/imdb/service/CastingPrincipalService.java
@@ -1,19 +1,20 @@
 package fr.diginamic.imdb.service;
 
 import fr.diginamic.imdb.entity.CastingPrincipal;
+import fr.diginamic.imdb.entity.CastingPrincipalId;
 import fr.diginamic.imdb.repository.CastingPrincipalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CastingPrincipalService extends AbstractService<CastingPrincipal, Integer> {
+public class CastingPrincipalService extends AbstractService<CastingPrincipal, CastingPrincipalId> {
 
     @Autowired
-    private CastingPrincipalRepository CastingPrincipal;
+    private CastingPrincipalRepository castingPrincipal;
 
     @Override
-    protected JpaRepository<CastingPrincipal, Integer> getRepository() {
-        return CastingPrincipal;
+    protected JpaRepository<CastingPrincipal, CastingPrincipalId> getRepository() {
+        return castingPrincipal;
     }
 }
