b0c83cf394fd1e98300bc46aad021bd0ebb373a8
kieransweetman <kikisweetman@gmail.com>
2024-08-29 11:23:36 +0200

    .

--- src/main/java/fr/diginamic/imdb/strategies/FilmsStrategy.java
+++ src/main/java/fr/diginamic/imdb/strategies/FilmsStrategy.java
@@ -1,6 +1,6 @@
 package fr.diginamic.imdb.strategies;
 
-public class FStrategy implements ICsvProcessingStrategy {
+public class FilmsStrategy implements ICsvProcessingStrategy {
     @Override
     public void process(String line) {
         // TODO Auto-generated method stub
