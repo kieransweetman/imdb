62f499efc50cf47d033b6a0310ab7d5a3ab58cc7
kieransweetman <kikisweetman@gmail.com>
2024-08-29 11:33:13 +0200

    fda

--- src/main/java/fr/diginamic/imdb/ImdbApplication.java
+++ src/main/java/fr/diginamic/imdb/ImdbApplication.java
@@ -3,12 +3,11 @@ package fr.diginamic.imdb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import fr.diginamic.imdb.utils.CsvFileRunner;
+import fr.diginamic.imdb.utils.CsvFileRunner;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "fr.diginamic.imdb")
 public class ImdbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ImdbApplication.class, args);
 	}
-
 }
