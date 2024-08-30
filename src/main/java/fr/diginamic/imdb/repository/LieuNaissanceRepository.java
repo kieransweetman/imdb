c90450aeadae26df13ff72b83c438ab72544d6c1
kieransweetman <kikisweetman@gmail.com>
2024-08-30 15:55:23 +0200

    updated lieunaissance repository

--- src/main/java/fr/diginamic/imdb/repository/LieuNaissanceRepository.java
+++ src/main/java/fr/diginamic/imdb/repository/LieuNaissanceRepository.java
@@ -4,9 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import fr.diginamic.imdb.entity.LieuNaissance;
-import fr.diginamic.imdb.entity.LieuNaissanceId;
 
 @Repository
-public interface LieuNaissanceRepository extends JpaRepository<LieuNaissance, LieuNaissanceId > {
+public interface LieuNaissanceRepository extends JpaRepository<LieuNaissance, Integer> {
 
 }
